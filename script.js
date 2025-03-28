d135a154b9934cbfbb7703687cd380adde44b8a9
Gaurav <gauravyadav2308@gmail.com>
2025-03-28 19:32:46 +0530

    save file

---
+++ script.js
@@ -0,0 +1,27 @@
+//1-> log your name and favorite hobby to the console.
+// console.log("Gaurav Yadav");
+// console.log("Cricket");
+
+//2-> Perform and log the result of 45*2 -10;
+// let ans = (45*2)-10
+// console.log(ans)
+
+//3-> Use console.log() to display the current year
+// let date = new Date()
+// let year = date.getFullYear()
+// console.log(year)
+
+//4-> Create two variable for first and last name. Concantenate and log them
+// let firstName = "Gaurav"
+// let lastName ="Yadav"
+// console.log(firstName+" "+lastName)
+
+//5-> Track the value of a variable by logging it before and after updating 
+// let ans=10
+// console.log(ans)
+// ans=15;
+// console.log(ans);
+
+//6-> Use console.error() to simulate an error message .
+let ans
+console.error(message)
