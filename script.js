5cc7e3ace24f619be14b18c88f4b24c1551fa3da
Gaurav <gauravyadav2308@gmail.com>
2025-03-29 09:40:26 +0530

    more questions

--- script.js
+++ script.js
@@ -16,11 +16,45 @@
 // let lastName ="Yadav"
 // console.log(firstName+" "+lastName)
 
-//5-> Track the value of a variable by logging it before and after updating 
+//5-> Track the value of a variable by logging it before and after updating
 // let ans=10
 // console.log(ans)
 // ans=15;
 // console.log(ans);
 
 //6-> Use console.error() to simulate an error message .
-console.error("error")
+// console.error("error")
+
+//7-> Log the square of the number 12 to the console.
+// console.log(Math.pow(12,2))
+
+//8-> Print the type of a variable holding the value true;
+// let ans=true;
+// console.log(typeof(ans))
+
+//9->Create a variable holding your age and log whethere it's greater than 18
+// let age = 50;
+// if (age > 18) {
+//   console.log(`Your age is greater the 18 , Your age is ${age}`);
+// } else {
+//   console.log("Your age less then 18");
+// }
+
+//10-> Log the result of 100/0 and  observe the output
+// console.log(100/0)
+
+//11-> Declare a variable using let and log its value
+// let ans =10;
+// console.log(ans)
+
+//12-> Create a constant to store the value of PI and log it 
+// const PI = 3.1457
+// console.log(PI)
+// console.log(Math.PI)
+
+//13-> Reassign a value to a variable declared with let and log the result 
+// let ans =10
+// ans=11
+// console.log(ans)
+
+//14->
